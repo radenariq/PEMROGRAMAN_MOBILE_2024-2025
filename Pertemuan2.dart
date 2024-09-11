594212502ebd2eb457142d7811578b54c739d6fa
RADEN ARIQ RESYA ALAUDDINE <ariqresya42@gmail.com>
2024-09-11 17:45:25 +0700

    pertemuan2

--- Pertemuan2.dart
+++ Pertemuan2.dart
@@ -1,5 +1,5 @@
-void main(){
-  for (int i = 19; i > 9; i--){
-    print ('Nama saya Fulan, sekarang berumur ${i - 1}');
+void main() {
+  for (int i = 19; i > 9; i--) {
+    print('Nama saya ariq, sekarang berumur ${i - 1}');
   }
 }
